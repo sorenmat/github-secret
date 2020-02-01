518e74d14a19a88134c3851d5f2ab5740b0314f6
Soren Mathiasen <smo@tradeshift.com>
2020-02-01 10:07:38 +0100

    Initial code commit
    Everything should be working

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/sorenmat/github-secret
+
+go 1.13
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dchest/blake2b v1.0.0 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/mdp/sodiumbox v0.1.0
+	github.com/stretchr/testify v1.4.0
+	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4 // indirect
+	golang.org/x/sys v0.0.0-20190813064441-fde4db37ae7a // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gopkg.in/yaml.v2 v2.2.4 // indirect
+)
---
+++ secret_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_publickey(t *testing.T) {
+	org := "sorenmat"
+	repo := "github-secret"
+
+	token := os.Getenv("GITHUB_TOKEN")
+	pk := GetPublickey(org, repo, token)
+	assert.NotNil(t, pk)
+
+	err := Updatesecret(org, repo, "my-super-secret", "testing", token)
+	assert.NoError(t, err)
+
+	secrets := GetSecrets(org, repo, token)
+	assert.NotNil(t, secrets)
+
+	secret := GetSecret(org, repo, "my-super-secret", token)
+	assert.NotNil(t, secret)
+	assert.Equal(t, "my-super-secret", secret.Name)
+
+	err = DeleteSecret(org, repo, "my-super-secret", token)
+	assert.NoError(t, err)
+
+}
